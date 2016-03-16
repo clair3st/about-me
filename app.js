b3318b7741a454564d81e4715fa133e8251cdc15
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-16 08:56:00 -0700

    first version, new folder location and first push

---
+++ app.js
@@ -0,0 +1,23 @@
+var firstName = prompt('What is your first name');
+alert('Your name is ' + firstName);
+console.log('First name: ' + firstName);
+
+var favouriteFood = prompt(firstName + ' What is your favourite food?');
+alert('Great! let\'s have ' + favouriteFood + ' for lunch');
+console.log('Favourite food: ' + favouriteFood);
+
+var favouriteColour = prompt('What is your favourite color?');
+alert('Nice! Your favourite colour is ' + favouriteColour + '. Mine is blue');
+console.log('Favourite colour: ' + favouriteColour);
+
+var birthTown = prompt('Where were you born?');
+alert('Cool, ' + birthTown + ' sounds like a nice place');
+console.log('Birth town: ' + birthTown);
+
+var schoolStudy = prompt('What did you study at school?');
+alert('Great, so you must be an expert in ' + schoolStudy);
+console.log('Subject of study: ' + schoolStudy);
+
+var holidayDestination = prompt('Where do you think I should go on holidays next?');
+alert('You think I should go to ' + holidayDestination + '? That stinks, I think I\'d rather stay at home');
+console.log('Holiday recomendation: ' + holidayDestination);
