693370be82008c0d5fb1f18b1e26663b9f1043f4
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-21 13:41:08 -0700

    removed commented out parts of code in app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ console.log('First name: ' + firstName);
 //initialise the score
 var questionArray = ['Firstly, was I born in the USA? Yes or No?', 'Do I have a pet cat? Yes or No?', 'Now you know I am from Australia, do you think I can surf?', 'Apart from Australia and the USA, do you think I have lived in any other countries?', 'I love Japanese food, more than just sushi. I could eat good ramen for the rest of my life. Do you agree with me?'];;
 var answerArray = [];
-var correctAnswerArray = [];
 
 //first question
 var pTagOne = document.getElementById('ans-one');
@@ -17,7 +16,6 @@ function birthPlaceQ() {
     pTagOne.textContent = 'I\'m sorry that wasn\'t right. I was born in Canberra, Australia.';
   } else if (birthPlace.toLowerCase() === 'no' || birthPlace.toLowerCase() === 'n') {
     pTagOne.textContent = 'Correct! I was born in Canberra, Australia.';
-    //userScore ++;
     answerArray.push(birthPlace.toLowerCase());
     console.log(answerArray);
   } else {
@@ -35,7 +33,6 @@ function petCatQ() {
     pTagTwo.textContent = 'Technically incorrect! I do have a cat that I am fostering for the Seattle Animal Shelter.';
   } else if (petCat.toLowerCase() === 'no' || petCat.toLowerCase() === 'n') {
     pTagTwo.textContent = 'Technically Correct! The cat I have belongs to the Seattle Animal Shelter and I am fostering it until it can be adopted.';
-    //userScore ++;
     answerArray.push(petCat.toLowerCase());
     console.log(answerArray);
   } else {
@@ -53,7 +50,6 @@ function hangTenQ(){
     pTagThree.textContent = 'I\'m sorry, incorrect. I grew up in Canberra which is a 2.5 hour drive from the nearest beach. Instead, I learnt how to ski when I was younger.';
   } else if (hangTen.toLowerCase() === 'no' || hangTen.toLowerCase() === 'n'){
     pTagThree.textContent = 'Correct! I grew up in Canberra which is a 2.5 hour drive from the nearest beach. Instead, I learnt how to ski when I was younger.';
-    //userScore ++;
     answerArray.push(hangTen.toLowerCase());
     console.log(answerArray);
   } else {
@@ -69,7 +65,6 @@ function homeCountriesQ(){
   console.log('Q4. Countries lived: ' + homeCountries);
   if (homeCountries.toLowerCase() === 'yes' || homeCountries.toLowerCase() === 'y') {
     pTagFour.textContent = 'Correct, I lived in London for 12 months between finishing high school and starting university.';
-    //userScore ++;
     answerArray.push(homeCountries.toLowerCase());
     console.log(answerArray);
   } else if (homeCountries.toLowerCase() === 'no' || homeCountries.toLowerCase() === 'n') {
@@ -87,7 +82,6 @@ function japaneseFoodQ(){
   console.log('Q5. Japanese food: ' + japaneseFood);
   if (japaneseFood.toLowerCase() === 'yes' || japaneseFood.toLowerCase() === 'y') {
     pTagFive.textContent = 'Nice! You should go to Japan, if you haven\'t already, and have the food there. It blew my mind!';
-    //userScore ++;
     answerArray.push(japaneseFood.toLowerCase());
     console.log(answerArray);
   } else if (japaneseFood.toLowerCase() === 'no' || japaneseFood.toLowerCase() === 'n'){
@@ -126,7 +120,7 @@ function numberGuessQ(){
 }
 numberGuessQ();
 
-/*// Question 7 bonus adding list of users favourite games
+// Question 7 bonus adding list of users favourite games
 function favoriteGameQ(){
   var favoriteGame = [];
   var gameList = 1;
@@ -137,29 +131,3 @@ function favoriteGameQ(){
   }
 }
 favoriteGameQ();
-
-//Attempt at stretch goal
-/*var questions = ['Firstly, was I born in the USA? Yes or No?', 'Do I have a pet cat? Yes or No?', 'Now you know I am from Australia, do you think I can surf?', 'Apart from Australia and the USA, do you think I have lived in any other countries?', 'I love Japanese food, more than just sushi. I could eat good ramen for the rest of my life. Do you agree with me?'];
-var numberOfQuestions = questions.length;
-var correctAnsWord = ['no', 'no', 'no', 'yes', 'yes'];
-var correctAnsLetter = ['n','n','n','y','y'];
-var incorrectAnsWord = ['yes','yes','yes','no','no'];
-var correctResponse = ['Correct! I was born in Canberra, Australia.', 'Technically correct! The cat I have belongs to the Seattle Animal Shelter and I am fostering it until it can be adopted.','Correct! I grew up in Canberra which is a 2.5 hour drive from the nearest beach. Instead, I learnt how to ski when I was younger.','Correct, I lived in London for 12 months between finishing high school and starting university.','Nice! You should go to Japan, if you haven\'t already, and have the food there. It blew my mind!' ];
-var incorrectResponse = ['I\'m sorry that wasn\'t right. I was born in Canberra, Australia.', 'Technically incorrect! I do have a cat that I am fostering for the Seattle Animal Shelter.','I\'m sorry, incorrect. I grew up in Canberra which is a 2.5 hour drive from the nearest beach. Instead, I learnt how to ski when I was younger.','Sorry, that\'s incorrect. I lived in London for 12 months between finishing high school and starting university.', 'Oh that\'s a shame. What is your favourite food?'];
-
-for (var i = 0; i < numberOfQuestions; i++) {
-  var userAnswer = prompt(questions[i]).toLowerCase();
-  console.log('user answer: ' + userAnswer);
-  console.log('i: ' + i);
-  if (userAnswer !== 'yes' && userAnswer !== 'no' && userAnswer !== 'n' && userAnswer !== 'y') {
-    alert('That was an invalid response.');
-  } else if (userAnswer === correctAnsWord[i] || userAnswer === correctAnsLetter[i]) {
-    alert(correctResponse[i]);
-    userScore ++;
-    console.log('user score: ' + userScore);
-  } else if (i === 4) {
-    prompt(incorrectResponse[i]);
-  } else {
-    alert(incorrectResponse[i]);
-  }
-}*/
